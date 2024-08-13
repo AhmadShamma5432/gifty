73e3930f2ffa539c4d4172d2f4488ba99ba9c57f
AhmadShamma <abogasan55555354@gmail.com>
2024-08-13 15:56:03 +0300

    dep commit

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,11 @@
+echo "BUILD START"
+# Ensure pip is installed
+python3 -m ensurepip --upgrade
+# Create and activate a virtual environment
+python3 -m venv venv
+source venv/bin/activate
+# Install dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+# Collect static files
+python3 manage.py collectstatic --noinput
+echo "BUILD END"
