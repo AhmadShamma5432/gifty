be730112a09e2bb0fa2cdd6b8057fe4e288b153b
AhmadShamma <abogasan55555354@gmail.com>
2024-08-23 17:36:06 +0300

    ..

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from djoser.views import UserViewSet
 from rest_framework.routers import DefaultRouter
 from django.urls import include,path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 from .views import CustomTokenObtainPairView,UserUpdateView
 
 
@@ -12,7 +12,7 @@ routers.register('user',UserUpdateView,basename='user')
 urlpatterns = [
     path('',include(routers.urls)),
     path(r'login',CustomTokenObtainPairView.as_view(),name='login'),
-   
+    path(r'refresh_token',TokenRefreshView.as_view(),name='refresh_token')
 ]
 
 
--- foodordering/urls.py
+++ foodordering/urls.py
@@ -22,10 +22,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/', include('base.urls')),
-    path('auth/',include('djoser.urls')),
     path('auth/',include('core.urls')),
-    path('auth/',include('djoser.urls.jwt')),
     path('__debug__/', include('debug_toolbar.urls')),
+    # path('auth/',include('djoser.urls')),
+    # path('auth/',include('djoser.urls.jwt')),
 ]  
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
